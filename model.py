929be1a8edb98797366a709959cf055df49e61d3
ekt4r <vercettiswims@gmail.com>
2026-07-06 11:16:47 +0300

    Step 57: compute_noam_learning_rate

--- model.py
+++ model.py
@@ -584,8 +584,10 @@ def shift_targets_right_with_start_token(target_ids, start_token_id):
         result.append(torch.concat((start_token_id, target[:-1])))
     return torch.stack(result)
 
-# Step 57 - compute_noam_learning_rate (not yet solved)
-# TODO: implement
+# Step 57 - compute_noam_learning_rate
+def compute_noam_learning_rate(step, d_model, warmup_steps):
+    # TODO: return the Noam warmup learning rate for the given step.
+    return d_model ** -0.5 * min(step ** -0.5, step * warmup_steps ** -1.5)
 
 # Step 58 - build_uniform_smoothing_distribution (not yet solved)
 # TODO: implement
